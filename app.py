ecb108eb43c49f1ef8c52ae4761b2bce40225cc0
jyaaan <john.yamashiro@gmail.com>
2024-11-29 09:50:44 -0800

    initializing postgres; adding base models

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 from const import DEBUG
 
 app = Flask(__name__)
+app.config.from_object("config.Config")
+db = SQLAlchemy(app)
 
 
 @app.route("/")
---
+++ server/models/base.py
@@ -0,0 +1,54 @@
+from typing import Optional, Type, TypeVar
+
+from sqlalchemy.ext.declarative import declarative_base, DeclarativeMeta
+from sqlalchemy import BigInteger, Column, DateTime, func, Integer
+
+DeclarativeBase: DeclarativeMeta = declarative_base()
+
+
+class BaseWithoutId(DeclarativeBase):  # type: ignore
+    __abstract__ = True
+
+    created = Column(
+        DateTime,
+        default=func.current_timestamp(),
+        nullable=False,
+    )
+    created_ts = Column(
+        Integer,
+        default=func.date_part("epoch", func.now()),
+        nullable=False,
+    )
+    last_modified = Column(
+        DateTime,
+        default=func.current_timestamp(),
+        onupdate=func.current_timestamp(),
+        nullable=False,
+    )
+    last_modified_ts = Column(
+        Integer,
+        default=func.date_part("epoch", func.now()),
+        onupdate=func.date_part("epoch", func.now()),
+        nullable=False,
+    )
+
+
+# BT = BaseTable
+BT = TypeVar("BT", bound="Base")
+
+
+class Base(BaseWithoutId):
+    __abstract__ = True
+
+    id = Column(
+        BigInteger,
+        primary_key=True,
+    )
+
+    @classmethod
+    def get_by_id(cls: Type[BT], id: int) -> Optional[BT]:
+        return cls.query.filter_by(id=id).first()
+
+    @classmethod
+    def get_by_id_strict(cls: Type[BT], id: int) -> BT:
+        return cls.query.filter_by(id=id).one()

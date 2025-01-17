c759db651bbc855869826abc4e5ae05e9e13de9e
jyaaan <john.yamashiro@gmail.com>
2025-01-17 11:32:48 -0800

    wrapping up for branching

---
+++ server/models/daily_image_version.py
@@ -0,0 +1,7 @@
+# s3_presigned_happy_url
+# s3_presigned_sad_url
+# date DDMMYY not datetime unique
+# active bool
+
+# visits the site -> pull latest version that's active,
+# return appropriate URL.
--- server/workers/run_worker.py
+++ server/workers/run_worker.py
@@ -1,8 +1,13 @@
 """ Generic worker because we don't want to pay for additional dynos we don't need """
 
 from apscheduler.schedulers.blocking import BlockingScheduler  # type: ignore
+from datetime import datetime
 
 from facades.cmc_facade import get_btc_price
+from openai_files.helpers import get_prompt, get_system_message
+from openai_files.openai_client import OpenAIClient
+from openai_files.utils import PromptType
+from server.s3_client import S3ClientFactory
 
 SESSION_BATCH_SIZE = 10
 
@@ -33,22 +38,21 @@ def check_bitcoin_price() -> None:
 # save pre-signed URLs
 @scheduler.scheduled_job("cron", hour=0, minute=1, timezone="America/Los_Angeles")
 def daily_image_generation() -> None:
-    # from server.s3_client import S3ClientFactory
+    s3_client = S3ClientFactory()
+    client = OpenAIClient()
 
-    # s3_client = S3ClientFactory()
-    # client = OpenAIClient()
-    # holiday_list = client.fetch_holiday_list()
-    # holiday_image_prompt = get_prompt(PromptType.GENERATE_IMAGE_HAPPY, holiday_list)
-    # print(holiday_image_prompt)
-    # holiday_image_url = client.fetch_generated_image_url(holiday_image_prompt)
-    # print(holiday_image_url)
-    # curr_date_str = datetime.now().strftime("%d-%b-%Y").upper()
-    # file_name = f"{PromptType.GENERATE_IMAGE_HAPPY.value}-{curr_date_str}"
-    # u_file_name = client.generate_unique_file_name(file_name)
-    # s3_image_url = s3_client.save_image_to_s3(holiday_image_url, u_file_name)
-    # print(f"{s3_image_url=}")
-    # pre_signed_url = s3_client.fetch_presigned_url(u_file_name)
-    # print(f"{pre_signed_url=}")
+    holiday_list = client.fetch_holiday_list()
+    holiday_image_prompt = get_prompt(PromptType.GENERATE_IMAGE_HAPPY, holiday_list)
+    print(holiday_image_prompt)
+    holiday_image_url = client.fetch_generated_image_url(holiday_image_prompt)
+    print(holiday_image_url)
+    curr_date_str = datetime.now().strftime("%d-%b-%Y").upper()
+    file_name = f"{PromptType.GENERATE_IMAGE_HAPPY.value}-{curr_date_str}"
+    u_file_name = client.generate_unique_file_name(file_name)
+    s3_image_url = s3_client.save_image_to_s3(holiday_image_url, u_file_name)
+    print(f"{s3_image_url=}")
+    pre_signed_url = s3_client.fetch_presigned_url(u_file_name)
+    print(f"{pre_signed_url=}")
     # write pre_signed_url x 2 to DB.
 
     # image table will be like:

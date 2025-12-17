45c69a75f78384d016bff66da063bc13aff76967
Clyde Autin <clydeautin@Clydes-MacBook-Pro-2.local>
2025-12-17 15:47:46 -0800

    added fixed url fallback in config.py

--- config.py
+++ config.py
@@ -6,7 +6,12 @@ load_dotenv()
 
 class Config:
 
-    uri = os.getenv("DATABASE_URL")
+    #Fallback to DATABASE_URL_FIXED if needed for Heroku compatibility
+    uri = os.getenv("DATABASE_URL") or os.getenv("DATABASE_URL_FIXED")
+
+    if not uri:
+        raise RuntimeError("DATABASE_URL not set")
+
     if uri and uri.startswith("postgres://"):
         uri = uri.replace("postgres://", "postgresql://", 1)
     SQLALCHEMY_DATABASE_URI = uri
